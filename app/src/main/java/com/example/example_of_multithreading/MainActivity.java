11d2c663001bd35873d5ad53c6741136d46171b5
minhaj.softifybd <minhaj.softifybd@gmail.com>
2021-08-24 01:11:32 +0600

    test

---
+++ app/src/main/java/com/example/example_of_multithreading/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.example_of_multithreading;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
